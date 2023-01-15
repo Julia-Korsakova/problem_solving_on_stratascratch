e0668263f96adef96c6f6ad8c372f665a3e4e397
Julia-Korsakova <pj.04.april@gmail.com>
2023-01-15 19:15:04 +0800

    Корректировка задачи Find all posts which were reacted to with a heart - изменено название колонки, по которой будут связаны датафреймы

--- Find all posts which were reacted to with a heart.py
+++ Find all posts which were reacted to with a heart.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-df_heart = facebook_reactions[facebook_reactions['reaction'].str.contains('heart')]['poster']
-df = pd.merge(df_heart, facebook_posts, how='inner', left_on = 'poster', right_on = 'post_id').drop_duplicates()
+df_heart = facebook_reactions[facebook_reactions['reaction'].str.contains('heart')]['post_id']
+df = pd.merge(df_heart, facebook_posts, how='inner', left_on = 'post_id', right_on = 'post_id').drop_duplicates()
