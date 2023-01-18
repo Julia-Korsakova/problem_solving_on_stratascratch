46081f5ff004b068daeb81a6cbe967f524fb1aa8
Julia-Korsakova <pj.04.april@gmail.com>
2023-01-18 09:54:33 +0800

    Корректировка задачи Number Of Bathrooms And Bedrooms - исправлены названия полей для расчета среднего значения

--- Number Of Bathrooms And Bedrooms.py
+++ Number Of Bathrooms And Bedrooms.py
@@ -1,4 +1,5 @@
 import pandas as pd
 import numpy as np
 
-airbnb_search_details.groupby(['city', 'property_type']).agg({'bedrooms': np.mean, 'beds': np.mean}).reset_index()
+airbnb_search_details.groupby(['city', 'property_type']).agg({'bathrooms': np.mean , 'bedrooms': np.mean})\
+    .reset_index()
